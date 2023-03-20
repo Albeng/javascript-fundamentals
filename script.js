977c74eb6f1b2cc8b9aac8038efbbe7d7e1b1880
Albeng <alvinalvaro146@gmail.com>
2023-03-20 21:45:38 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//  let admin;
+//  let name = "John";
+
+//  admin = name;
+
+//  alert(admin);
+
+// let thePlanetEarth = "Earth";
+// let currentUserName = "John";
+
+// const BIRTHDAY = "18.04.1982";
+// let age = someCode(BIRTHDAY);
+
+// let x = 10 / 2;
+
+// alert(x)
+
+// String Concatenation
+
+// let x = "10";
+// let y = "20";
+// let z = x + y;
+
+// console.log(z);
+
+
+// 
+
+let x = 10;
+let y = 10;
+let z = x === y;
+
+console.log(z);
+
+// assignment (=)
+// equality (==)
+// strict equality (===)

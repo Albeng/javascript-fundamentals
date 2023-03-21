7826141d0025d4c28f0b2b54be891ff93a96a0ab
Albeng <alvinalvaro146@gmail.com>
2023-03-21 20:50:28 +0800

    Update

--- script.js
+++ script.js
@@ -26,12 +26,67 @@
 
 // 
 
-let x = 10;
-let y = 10;
-let z = x === y;
+// let x = 10;
+// let y = 10;
+// let z = x === y;
 
-console.log(z);
+// console.log(z);
 
 // assignment (=)
 // equality (==)
 // strict equality (===)
+
+// const string = "The revolution will not be televised.";
+
+// console.log(string);
+
+// const singleQuote = 'Single Quotes.';
+// const doubleQuote = "Double Quotes.";
+
+// console.log (singleQuote);
+// console.log (doubleQuote);
+
+// const name = "Alvin";
+// const greeting = `Hello, ${name}!`;
+
+// console.log(greeting);
+
+
+
+// function greet() {
+//     // Declare the name
+//     const name = promt("What is Your Name?");
+//     // Print the name to the console
+//     console.log(name);
+// }
+
+// greet();
+
+// - String type methods in JavaScript
+//slice method
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12, -6);
+
+// console.log(part);
+
+// substring method
+
+// let str = "Apple, Banana, Kiwi";
+// let fruit = str.substring(7, 13);
+
+// console.log(fruit);
+
+// Replace Method
+
+// let newText = "Please visit Microsoft";
+// let updatedText = newText.replace("Microsoft", "KodeGo");
+
+// console.log(updatedText);
+
+// Conditionals
+
+// 1 > 2;
+// 2 > 1;
+// 1 == 1;
+// 1 === "1";
+// 1 != 1;
